13234cf8681c1c4a4f8b44424c7caf92c441b604
Mohammed Qasim Charolia <MohammedQasim.Charolia@lntinfotech.com>
2018-12-15 16:44:28 +0530

    Nwe commit for git push

--- GitDemo/src/Application.java
+++ GitDemo/src/Application.java
@@ -4,8 +4,8 @@ public class Application {
 	public static void main(String[] args) {
 		
 		// TODO Auto-generated method stub
-		System.out.println("Hello India.");
-		System.out.println("Hello World.");
+		System.out.println("Hello India._Quin");
+		System.out.println("Hello World._Quin");
 		
 	}
 
