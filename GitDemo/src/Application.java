bf4f26574e3889a40a6c03accf7d1599c2645460
Mohammed Qasim Charolia <MohammedQasim.Charolia@lntinfotech.com>
2018-12-15 15:26:24 +0530

    New Commit on dev

--- GitDemo/src/Application.java
+++ GitDemo/src/Application.java
@@ -2,8 +2,11 @@
 public class Application {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
-		System.out.println("Hello India3");
+		System.out.println("Hello India.");
+		System.out.println("Hello World.");
+		
 	}
 
 }
